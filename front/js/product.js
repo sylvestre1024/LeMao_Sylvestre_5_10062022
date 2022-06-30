f063a1a247d5e104f06ea63e06f8522b9bfdebef
Sylvestre <sylvestre1024@gmail.com>
2022-06-30 19:11:12 +0200

    Maj 02 - Retravail global selon remarques et 2 fonctions communes jusquà ligne 56 (indentation, nom de variable avec du sens, pas de contrôle sur données venant de l'API

--- front/js/product.js
+++ front/js/product.js
@@ -1,149 +1,132 @@
-const c_myProtocol = "http://";
-const c_myHost = "localhost"
-const c_myPort = ":3000";
-const c_myAddress = "/api/products";
-const c_fullAddress = c_myProtocol + c_myHost + c_myPort + c_myAddress;
-const c_allProduct = c_fullAddress + "/";
-//const c_numOrder = c_fullAddress + "/order";
-
-
-
+const var_protocol = "http";
+const var_host = "://localhost"
+const var_port = ":3000";
+const var_path = "/api/products";
+const var_fullAddress = var_protocol + var_host + var_port + var_path;
+const var_allProducts = var_fullAddress + "/";
 
 
 /**
  * Retourne du contenu de API
- * @param { string } a // qui est un chemin url
- * @param { string } p // qui est une chaine de(s) parametre(s)
+ * @param { string } param_path // qui est un chemin url
+ * @param { string } param_keyAndValue // qui est une chaine de(s) parametre(s)
  */
-async function f_retrieveContent(a, p) {
-    let response = await fetch(a + p);
+async function retrieveContent(param_path, param_keyAndValue) {
+    if (param_keyAndValue == undefined) {
+        param_keyAndValue = '';
+    }
+    const response = await fetch(param_path + param_keyAndValue);
     return response.json();
+    // !!! Demande du mentor : faire evoluer avec un timeout !!!
 };
 
+
 /**
- * Utilise la bonne route pour les contenues Json receptionne
- * @param { string } b // qui est un chemin url
- * @param { string } p // qui est une chaine de(s) parametre(s)
+ * Utilise le bon iteneraire selon la demande
+ * @param { string } routeAsked
  */
-async function f_readContent(b, p) {                                        //a ameliorer une chaine peut etre constitue de plusieurs couples de valeurs
+async function hubContent(routeAsked) {
     try {
-        // on supprime dans p la partie "id=" puisque API fonctionne de cette maniere
-        let v_numId = p.replace('id=', '');
-        console.log("Remplacement de la chaine id sans prefixe : " + v_numId);
-
-        let v_content = await f_retrieveContent(b, v_numId); // chemin et id
-        console.log(v_content);
-
-        //CAS : tous les produits
-        if (b = c_allProduct) {
-            //console.log(v_content);
-            //readProduct(v_content);
-        };
-
-        //CAS : detail du produit
-        // test si parametre id correct
-        let v_testParamId = false;
-        let v_regexParamId = new RegExp('^id=[a-zA-Z0-9]+$'); //  Une chaîne de caractères alpha-numérique en debut jusqu a fin
-        // si p contient la regex
-        if (v_regexParamId.test(p)) {
-            v_testParamId = true;
-            //console.log("test regex avant appel API");
-            //console.log("b :" + b + " compare c_allProduct : " + c_allProduct);
-        };
-        if (b == c_allProduct && v_testParamId == true) {
-            //console.log("test avant appel API suite");
-            f_readSelectedProduct(v_content);
-        };
-
+        if (routeAsked !== undefined) {
+            //recupere le contenu via iteneraire determinee par Api   
+            if (routeAsked == "routeSelectedProduct") {
+                //reprend la valeur du parametre id dans Url
+                const var_idUrl = idGetRequest();
+                if (var_idUrl != "" && var_idUrl != undefined) {
+                    hubContent(var_fullAddress + "/", "id=" + var_idUrl, "routeProduct");
+                    //cherche le contenu de API
+                    content = await retrieveContent(var_allProducts, var_idUrl);
+                    //ecrit le contenu
+                    writeSelectedProduct(content);
+                } else {
+                    console.log("id avec format incorrect ou vide");
+                }
+            }
+            if (routeAsked == "routeAllProducts") {
+                //cherche le contenu de API
+                content = await retrieveContent(var_allProducts);
+                //ecrit le contenu
+                writeAllProducts(content);
+            }
+        }
     } catch (e) {
-        console.log('Error', e);
-    };
+        console.log('Catched Error', e);
+        console.log(routeAsked);
+    }
 };
-
-
-// fonction recupere id provenant de Url
-function f_idGetRequest() {
-
-    let v_str = window.location.href;
-    let v_url = new URL(v_str);
-    let v_search_params = new URLSearchParams(v_url.search);
-    let iobj_regex = new RegExp('^[a-zA-Z0-9]+$'); //  Une chaîne de caractères alpha-numérique en debut jusqu a fin
-
-    if (v_search_params.has('id')) {
-
-        let v_id = v_search_params.get('id');
-        console.log(v_id)
-
-        if (iobj_regex.test(v_id)) {
-            // appelle la fonction affichage du Detail Produit
-            console.log("id existant et au bon format");
-            return v_id;
-
-            // sinon sort avec un format incorrect
-        } else {
-            console.log("id avec format incorrect");
-        };
-    };
-};
-
 
 /**
- * Formate le contenu Json de l API pour la lecture du Detail du produit
- * @param { Json } c
+ * Ecrit le contenu du produit selectionne dans la page html
+ * @param { array } array_selectedProduct
  */
-async function f_readSelectedProduct(c) {
+async function writeSelectedProduct(array_selectedProduct) {
     try {
-        //console.log(c);
-
+        
         //titre de la page html
-        document.title = c.name;
-
+        document.title = array_selectedProduct.name;
+        
         //image
-        let v_elt01 = document.createElement('img');
-        v_elt01.setAttribute("src", c.imageUrl);
-        v_elt01.setAttribute("alt", c.altTxt);
-        document.getElementsByClassName('item__img')[0].appendChild(v_elt01);       //a simplifier ?
-
+        const var_image = document.createElement('img');
+        var_image.setAttribute("src", array_selectedProduct.imageUrl);
+        var_image.setAttribute("alt", array_selectedProduct.altTxt);
+        document.getElementsByClassName('item__img')[0].appendChild(var_image);       //a simplifier ?
+        
         //titre
-        let v_elt02 = document.getElementById('title');                             //a simplifier ?
-        v_elt02.innerHTML = c.name;
+        const var_title = document.getElementById('title');                             //a simplifier ?
+        var_title.innerHTML = array_selectedProduct.name;
 
         //prix
-        let v_elt03 = document.getElementById('price');                             //a simplifier ?
-        v_elt03.innerHTML = c.price;
-
+        const var_price = document.getElementById('price');                             //a simplifier ?
+        var_price.innerHTML = array_selectedProduct.price;
+        
         //description
-        let v_elt04 = document.getElementById('description');                       //a simplifier ?
-        v_elt04.innerHTML = c.description;
+        const var_description = document.getElementById('description');                       //a simplifier ?
+        var_description.innerHTML = array_selectedProduct.description;
 
         //couleurs
-        let v_eltHtmlSelect = c.colors;
+        const var_eltHtmlSelect = array_selectedProduct.colors;
         //boucle de remplissage des options de couleur
-        for (var i = 0; i < v_eltHtmlSelect.length; i++) {
+        for (var i = 0; i < var_eltHtmlSelect.length; i++) {
             //pour une option
-            let v_elt05 = document.createElement('option');
-            v_elt05.setAttribute("value", v_eltHtmlSelect[i]);
-            v_elt05.innerHTML = v_eltHtmlSelect[i];
-            document.getElementById('colors').appendChild(v_elt05);                 //a simplifier ?
+            const var_color = document.createElement('option');
+            var_color.setAttribute("value", var_eltHtmlSelect[i]);
+            var_color.innerHTML = var_eltHtmlSelect[i];
+            document.getElementById('colors').appendChild(var_color);                 //a simplifier ?
         };
 
         //quantite unitaire
-        let v_elt06 = document.getElementById('quantity');                          //a simplifier ?
-        v_elt06.setAttribute("value", 1);
-
+        const var_quantity = document.getElementById('quantity');                          //a simplifier ?
+        var_quantity.setAttribute("value", 1);
+        
     } catch (c) {
-        console.log('Error', c);
+        console.log('Catched Error', array_selectedProduct);
     };
 };
 
-//Charge la requete GET avec ID valide sur page product.html
-let v_idUrl = f_idGetRequest();
-if (v_idUrl != "" && v_idUrl != undefined) {
-    f_readContent(c_fullAddress + "/", "id=" + v_idUrl);
-} else {
-    console.log("id avec format incorrect ou vide");
+
+// Recupere id provenant de Url et verifie son format
+function idGetRequest() {
+    const var_str = window.location.href;
+    const myUrl = new URL(var_str);
+    const var_search = new URLSearchParams(myUrl.search);
+    // une chaine de caracteres alpha-numerique en debut jusqu a fin
+    const myRegex = new RegExp('^[a-zA-Z0-9]+$');
+    //si parametre id existant dans url
+    if (var_search.has('id')) {
+        const var_id = var_search.get('id');
+        //si format id au norme
+        if (myRegex.test(var_id)) {
+            return var_id;
+        } else {
+            console.log("id avec format incorrect");
+        }
+    }
 };
 
+
+// partie en execution
+// appelle le contenu des Produits de l API et insere Html
+hubContent("routeSelectedProduct");
 
 
 //document.getElementById('addToCard')
@@ -151,17 +134,79 @@ if (v_idUrl != "" && v_idUrl != undefined) {
 
 
 //ajoute des ecouteurs a notre bouton Ajouter au panier
-let v_btnCart = document.getElementById("addToCart");                              //a simplifier ?
-v_btnCart.addEventListener("click", () => {
+const var_btnCart = document.getElementById("addToCart");
+var_btnCart.addEventListener("click", () => {
     //creation object articleSelected si les conditions sont correctes
-    let obj_articleSelected = {
-        id: v_idUrl,
+    const obj_articleSelected = {
+        id: var_idUrl,
         quantity: document.getElementById('quantity').value,                       //a simplifier ?
         color: document.getElementById('colors').value,                            //a simplifier ?
         name: document.getElementById('title').textContent                         //a simplifier ?
     };
     console.log(obj_articleSelected);
-    //f_verifyInput(articleSelected);
+    //verifyInput(articleSelected);
 });
 
+/*
+async function readContent(b, p) {                                        //a ameliorer une chaine peut etre constitue de plusieurs couples de valeurs
+    try {
+        // on supprime dans p la partie "id=" puisque API fonctionne de cette maniere
+        const var_numId = p.replace('id=', '');
+        console.log("Remplacement de la chaine id sans prefixe : " + var_numId);
 
+        const var_content = await retrieveContent(b, var_numId); // chemin et id
+        console.log(var_content);
+
+        //CAS : tous les produits
+        if (b = var_allProduct) {
+            //console.log(var_content);
+            //readProduct(var_content);
+        };
+
+        //CAS : detail du produit
+        // test si parametre id correct
+        const var_testParamId = false;
+        const var_regexParamId = new RegExp('^id=[a-zA-Z0-9]+$'); //  Une chaîne de caractères alpha-numérique en debut jusqu a fin
+        // si p contient la regex
+        if (var_regexParamId.test(p)) {
+            var_testParamId = true;
+            //console.log("test regex avant appel API");
+            //console.log("b :" + b + " compare var_allProduct : " + var_allProduct);
+        };
+        if (b == var_allProduct && var_testParamId == true) {
+            //console.log("test avant appel API suite");
+            readSelectedProduct(var_content);
+        };
+
+    } catch (e) {
+        console.log('Catched Error', e);
+    };
+};
+*/
+
+/*
+// fonction recupere id provenant de Url
+function idGetRequest() {
+
+    const var_str = window.location.href;
+    const var_url = new URL(var_str);
+    const var_search_params = new URLSearchParams(var_url.search);
+    const iobj_regex = new RegExp('^[a-zA-Z0-9]+$'); //  Une chaîne de caractères alpha-numérique en debut jusqu a fin
+
+    if (var_search_params.has('id')) {
+
+        const var_id = var_search_params.get('id');
+        console.log(var_id)
+
+        if (iobj_regex.test(var_id)) {
+            // appelle la fonction affichage du Detail Produit
+            console.log("id existant et au bon format");
+            return var_id;
+
+            // sinon sort avec un format incorrect
+        } else {
+            console.log("id avec format incorrect");
+        };
+    };
+};
+*/
--- front/js/script.js
+++ front/js/script.js
@@ -1,85 +1,105 @@
-const myProtocol = "http://";
-const myHost = "localhost"
-const myPort = ":3000";
-const myAddress = "/api/products";
-const fullAddress = myProtocol + myHost + myPort + myAddress;
-const allProduct = fullAddress + "/";
-//const numOrder = fullAddress + "/order";
+const var_protocol = "http";
+const var_host = "://localhost"
+const var_port = ":3000";
+const var_path = "/api/products";
+const var_fullAddress = var_protocol + var_host + var_port + var_path;
+const var_allProducts = var_fullAddress + "/";
 
 
 /**
  * Retourne du contenu de API
- * @param { Json } a
+ * @param { string } param_path // qui est un chemin url
+ * @param { string } param_keyAndValue // qui est une chaine de(s) parametre(s)
  */
-async function retrieveContent(a) {
-    let response = await fetch(a);
+async function retrieveContent(param_path, param_keyAndValue) {
+    if (param_keyAndValue == undefined) {
+        param_keyAndValue = '';
+    }
+    const response = await fetch(param_path + param_keyAndValue);
     return response.json();
+    // !!! Demande du mentor : faire evoluer avec un timeout !!!
 };
 
 
 /**
- * Utilise la bonne route pour les contenues Json receptionne
- * @param { Json } b
+ * Utilise le bon iteneraire selon la demande
+ * @param { string } routeAsked
  */
-async function readContent(b) {
-  try {
-    let content = await retrieveContent(b);
-      //console.log(content);
-
-      //cas : Tous les produits
-      if (b = "allProduct") {
-          //console.log(content);
-          readProduct(content);
-      };
-
-  } catch (b) {
-    console.log('Error', b);
-    };
-};
-
-
-/**
- * Formate le contenu Json de l API pour la lecture des produits
- * @param { Json } c
- */
-async function readProduct(c) {
+async function hubContent(routeAsked) {
     try {
-        console.log(c);
-
-        for (var i = 0; i < c.length; i++) {
-        
-        //lien
-        let elt01 = document.createElement('a');
-        elt01.setAttribute("href", "product.html?id=" + c[i]._id);
-        document.getElementById('items').appendChild(elt01);
-
-        //article
-        let elt02 = document.createElement('article');
-        elt01.appendChild(elt02);
-
-        //image
-        let elt03 = document.createElement('img');
-        elt03.setAttribute("src", c[i].imageUrl);
-        elt03.setAttribute("alt", c[i].altTxt);
-        elt02.appendChild(elt03);
-
-        //titre
-        let elt04 = document.createElement('h3');
-        elt04.setAttribute("class", "productName");
-        elt04.innerHTML = c[i].name;
-        elt02.appendChild(elt04);
-
-        //paragraphe
-        let elt05 = document.createElement('p');
-        elt05.setAttribute("class", "productDescription");
-        elt05.innerHTML = c[i].description;
-        elt02.appendChild(elt05);
-        };
-
-    } catch (c) {
-        console.log('Error', c);
-    };
+        if (routeAsked !== undefined) {
+            //recupere le contenu via iteneraire determinee par Api   
+            if (routeAsked == "routeSelectedProduct") {
+                //reprend la valeur du parametre id dans Url
+                const var_idUrl = idGetRequest();
+                if (var_idUrl != "" && var_idUrl != undefined) {
+                    hubContent(var_fullAddress + "/", "id=" + var_idUrl, "routeProduct");
+                    //cherche le contenu de API
+                    content = await retrieveContent(var_allProducts, var_idUrl);
+                    //ecrit le contenu
+                    writeSelectedProduct(content);
+                } else {
+                    console.log("id avec format incorrect ou vide");
+                }
+            }
+            if (routeAsked == "routeAllProducts") {
+                //cherche le contenu de API
+                content = await retrieveContent(var_allProducts);
+                //ecrit le contenu
+                writeAllProducts(content);
+            }
+        }
+    } catch (e) {
+        console.log('Catched Error', e);
+        console.log(routeAsked);
+    }
 };
 
-//Appelle le contenu des Produits de l API et insere Html
-readContent(allProduct);
+
+/**
+ * Ecrit le contenu de tous les produits dans la page html
+ * @param { array } array_textRequest
+ */
+async function writeAllProducts(array_textRequest) {
+    try {
+        //console.log(array_textRequest);
+
+        for (var i = 0; i < array_textRequest.length; i++) {
+        
+            //lien
+            const var_link = document.createElement('a');
+            var_link.setAttribute("href", "product.html?id=" + array_textRequest[i]._id);
+            document.getElementById('items').appendChild(var_link);
+
+            //article
+            const var_article = document.createElement('article');
+            var_link.appendChild(var_article);
+
+            //image
+            const var_image = document.createElement('img');
+            var_image.setAttribute("src", array_textRequest[i].imageUrl);
+            var_image.setAttribute("alt", array_textRequest[i].altTxt);
+            var_article.appendChild(var_image);
+
+            //titre
+            const var_title = document.createElement('h3');
+            var_title.setAttribute("class", "productName");
+            var_title.innerHTML = array_textRequest[i].name;
+            var_article.appendChild(var_title);
+
+            //description
+            const var_description = document.createElement('p');
+            var_description.setAttribute("class", "productDescription");
+            var_description.innerHTML = array_textRequest[i].description;
+            var_article.appendChild(var_description);
+            }
+
+    } catch (e) {
+        console.log('Catched Error', e);
+    }
+};
+
+
+// partie en execution
+// appelle le contenu des Produits de l API et insere Html
+hubContent("routeAllProducts");
