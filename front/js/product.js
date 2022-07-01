755519105ee3ebe97d725d3b535e7ce3fde9a413
Sylvestre <sylvestre1024@gmail.com>
2022-07-01 17:44:20 +0200

    Maj 02 - Mise en commun de script.js et product.js - prêt à fusionner

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+// CONFIGURATION GLOBALE
 const var_protocol = "http";
 const var_host = "://localhost"
 const var_port = ":3000";
@@ -6,6 +7,7 @@ const var_fullAddress = var_protocol + var_host + var_port + var_path;
 const var_allProducts = var_fullAddress + "/";
 
 
+// BIBLIOTHEQUE COMMUNE Debut _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 /**
  * Retourne du contenu de API
  * @param { string } param_path // qui est un chemin url
@@ -15,11 +17,47 @@ async function retrieveContent(param_path, param_keyAndValue) {
     if (param_keyAndValue == undefined) {
         param_keyAndValue = '';
     }
-    const response = await fetch(param_path + param_keyAndValue);
-    return response.json();
-    // !!! Demande du mentor : faire evoluer avec un timeout !!!
+    try {
+        const response = await fetch(param_path + param_keyAndValue);
+        return await response.json();
+    } catch (error) {
+        console.log('Catched Error5', error);
+        console.log('La reponse de API est en erreur avec ces parametres : ' + param_path + param_keyAndValue);
+    }
+}
+
+
+/**
+ * Identifie le bon itineraire vers SelectedProduct si le contexte url est valide
+ */
+function testRouteSelectedProduct() {
+    //reprend la valeur du parametre id dans Url
+    const var_idUrl = idGetRequest();
+    const testExpression = ("id=" + var_idUrl)
+    if (var_idUrl) {
+        //console.log('La page product.html contient bien un parametre id valide : ' + var_idUrl);
+        return true;
+    } else {
+        //console.log('La page product.html ne contient pas de parametre id valide : ' + var_idUrl);
+        return false;
+    }
 };
 
+/**
+ * Verifie que la page en cours est index.html
+ */
+function testRouteAllProducts() {
+    const myUrl = new URL(window.location);
+    const myRegex = new RegExp('index.html$');
+    const myUrlPathname = myUrl.pathname
+    //si trouve chaine
+    if (myRegex.test(myUrlPathname)) {
+        return true;
+    } else {
+        //console.log('La Regex echoue pour identifier la page index.html : ' + myUrlPathname);
+        return false;
+    }
+};
 
 /**
  * Utilise le bon iteneraire selon la demande
@@ -27,84 +65,29 @@ async function retrieveContent(param_path, param_keyAndValue) {
  */
 async function hubContent(routeAsked) {
     try {
-        if (routeAsked !== undefined) {
-            //recupere le contenu via iteneraire determinee par Api   
-            if (routeAsked == "routeSelectedProduct") {
-                //reprend la valeur du parametre id dans Url
-                const var_idUrl = idGetRequest();
-                if (var_idUrl != "" && var_idUrl != undefined) {
-                    hubContent(var_fullAddress + "/", "id=" + var_idUrl, "routeProduct");
-                    //cherche le contenu de API
-                    content = await retrieveContent(var_allProducts, var_idUrl);
-                    //ecrit le contenu
-                    writeSelectedProduct(content);
-                } else {
-                    console.log("id avec format incorrect ou vide");
-                }
-            }
-            if (routeAsked == "routeAllProducts") {
-                //cherche le contenu de API
-                content = await retrieveContent(var_allProducts);
-                //ecrit le contenu
-                writeAllProducts(content);
-            }
+        //recupere le contenu via iteneraire determinee par Api   
+        if (routeAsked == "routeSelectedProduct") {
+            //reprend la valeur du parametre id dans Url
+            const var_idUrl = idGetRequest();
+            content = await retrieveContent(var_allProducts, var_idUrl);
+            //ecrit le contenu
+            writeSelectedProduct(content);
+        }
+        if (routeAsked == "routeAllProducts") {
+            //cherche le contenu de API
+            content = await retrieveContent(var_allProducts);
+            //ecrit le contenu
+            writeAllProducts(content);
         }
     } catch (e) {
-        console.log('Catched Error', e);
-        console.log(routeAsked);
+        console.log('Catched Error4', e);
+        console.log('Erreur dans demande d itineraire ' + routeAsked);
     }
 };
 
 /**
- * Ecrit le contenu du produit selectionne dans la page html
- * @param { array } array_selectedProduct
+ * Recupere id provenant de Url et verifie son format
  */
-async function writeSelectedProduct(array_selectedProduct) {
-    try {
-        
-        //titre de la page html
-        document.title = array_selectedProduct.name;
-        
-        //image
-        const var_image = document.createElement('img');
-        var_image.setAttribute("src", array_selectedProduct.imageUrl);
-        var_image.setAttribute("alt", array_selectedProduct.altTxt);
-        document.getElementsByClassName('item__img')[0].appendChild(var_image);       //a simplifier ?
-        
-        //titre
-        const var_title = document.getElementById('title');                             //a simplifier ?
-        var_title.innerHTML = array_selectedProduct.name;
-
-        //prix
-        const var_price = document.getElementById('price');                             //a simplifier ?
-        var_price.innerHTML = array_selectedProduct.price;
-        
-        //description
-        const var_description = document.getElementById('description');                       //a simplifier ?
-        var_description.innerHTML = array_selectedProduct.description;
-
-        //couleurs
-        const var_eltHtmlSelect = array_selectedProduct.colors;
-        //boucle de remplissage des options de couleur
-        for (var i = 0; i < var_eltHtmlSelect.length; i++) {
-            //pour une option
-            const var_color = document.createElement('option');
-            var_color.setAttribute("value", var_eltHtmlSelect[i]);
-            var_color.innerHTML = var_eltHtmlSelect[i];
-            document.getElementById('colors').appendChild(var_color);                 //a simplifier ?
-        };
-
-        //quantite unitaire
-        const var_quantity = document.getElementById('quantity');                          //a simplifier ?
-        var_quantity.setAttribute("value", 1);
-        
-    } catch (c) {
-        console.log('Catched Error', array_selectedProduct);
-    };
-};
-
-
-// Recupere id provenant de Url et verifie son format
 function idGetRequest() {
     const var_str = window.location.href;
     const myUrl = new URL(var_str);
@@ -118,95 +101,141 @@ function idGetRequest() {
         if (myRegex.test(var_id)) {
             return var_id;
         } else {
-            console.log("id avec format incorrect");
+            console.log('La valeur de id est incorrecte dans son format : ' + var_id);
         }
     }
+    return false;
 };
+// BIBLIOTHEQUE COMMUNE Fin _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
 
-// partie en execution
-// appelle le contenu des Produits de l API et insere Html
-hubContent("routeSelectedProduct");
-
-
-//document.getElementById('addToCard')
-//document.addEventListener("click", addToCard);
-
-
-//ajoute des ecouteurs a notre bouton Ajouter au panier
-const var_btnCart = document.getElementById("addToCart");
-var_btnCart.addEventListener("click", () => {
-    //creation object articleSelected si les conditions sont correctes
-    const obj_articleSelected = {
-        id: var_idUrl,
-        quantity: document.getElementById('quantity').value,                       //a simplifier ?
-        color: document.getElementById('colors').value,                            //a simplifier ?
-        name: document.getElementById('title').textContent                         //a simplifier ?
-    };
-    console.log(obj_articleSelected);
-    //verifyInput(articleSelected);
-});
-
-/*
-async function readContent(b, p) {                                        //a ameliorer une chaine peut etre constitue de plusieurs couples de valeurs
+// BIBLIOTHEQUE SPECIFIQUE - index.html - Debut  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+/**
+ * Ecrit le contenu de tous les produits dans la page html
+ * @param { array } array_textRequest
+ */
+async function writeAllProducts(array_textRequest) {
     try {
-        // on supprime dans p la partie "id=" puisque API fonctionne de cette maniere
-        const var_numId = p.replace('id=', '');
-        console.log("Remplacement de la chaine id sans prefixe : " + var_numId);
+        // for (var i = 0; i < array_textRequest.length; i++) {     //OLD SCHOOL
+        array_textRequest.forEach((item) => {
 
-        const var_content = await retrieveContent(b, var_numId); // chemin et id
-        console.log(var_content);
+            //lien
+            const var_link = document.createElement('a');
+            var_link.setAttribute("href", "product.html?id=" + item._id);
+            document.getElementById('items').appendChild(var_link);
 
-        //CAS : tous les produits
-        if (b = var_allProduct) {
-            //console.log(var_content);
-            //readProduct(var_content);
-        };
+            //article
+            const var_article = document.createElement('article');
+            var_link.appendChild(var_article);
 
-        //CAS : detail du produit
-        // test si parametre id correct
-        const var_testParamId = false;
-        const var_regexParamId = new RegExp('^id=[a-zA-Z0-9]+$'); //  Une chaîne de caractères alpha-numérique en debut jusqu a fin
-        // si p contient la regex
-        if (var_regexParamId.test(p)) {
-            var_testParamId = true;
-            //console.log("test regex avant appel API");
-            //console.log("b :" + b + " compare var_allProduct : " + var_allProduct);
-        };
-        if (b == var_allProduct && var_testParamId == true) {
-            //console.log("test avant appel API suite");
-            readSelectedProduct(var_content);
-        };
+            //image
+            const var_image = document.createElement('img');
+            var_image.setAttribute("src", item.imageUrl);
+            var_image.setAttribute("alt", item.altTxt);
+            var_article.appendChild(var_image);
+
+            //titre
+            const var_title = document.createElement('h3');
+            var_title.setAttribute("class", "productName");
+            var_title.innerHTML = item.name;
+            var_article.appendChild(var_title);
+
+            //description
+            const var_description = document.createElement('p');
+            var_description.setAttribute("class", "productDescription");
+            var_description.innerHTML = item.description;
+            var_article.appendChild(var_description);
+        });
 
     } catch (e) {
-        console.log('Catched Error', e);
+        console.log('Catched Error2', e);
+        console.log('Erreur en ecriture dans la page index.html');
+    }
+};
+// BIBLIOTHEQUE SPECIFIQUE - index.html - Fin  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+// BIBLIOTHEQUE SPECIFIQUE - product.html - Debut  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+/**
+ * Ecrit le contenu du produit selectionne dans la page html
+ * @param { array } array_selectedProduct
+ */
+async function writeSelectedProduct(array_selectedProduct) {
+    try {
+
+        //titre de la page html
+        document.title = array_selectedProduct.name;
+
+        //image
+        const var_image = document.createElement('img');
+        var_image.setAttribute("src", array_selectedProduct.imageUrl);
+        var_image.setAttribute("alt", array_selectedProduct.altTxt);
+        document.getElementsByClassName('item__img')[0].appendChild(var_image);         //a simplifier ?
+
+        //titre
+        const var_title = document.getElementById('title');                             //a simplifier ?
+        var_title.innerHTML = array_selectedProduct.name;
+
+        //prix
+        const var_price = document.getElementById('price');                             //a simplifier ?
+        var_price.innerHTML = array_selectedProduct.price;
+
+        //description
+        const var_description = document.getElementById('description');                //a simplifier ?
+        var_description.innerHTML = array_selectedProduct.description;
+
+        //couleurs
+        const var_eltHtmlSelect = array_selectedProduct.colors;
+        //boucle de remplissage des options de couleur
+        //for (var i = 0; i < var_eltHtmlSelect.length; i++) {     //OLD SCHOOL
+        var_eltHtmlSelect.forEach((item) => {
+            //pour une option
+            const var_color = document.createElement('option');
+            var_color.setAttribute("value", item);
+            var_color.innerHTML = item;
+            document.getElementById('colors').appendChild(var_color);                 //a simplifier ?
+        });
+
+        //quantite unitaire
+        const var_quantity = document.getElementById('quantity');                     //a simplifier ?
+        var_quantity.setAttribute("value", 1);
+
+    } catch (error) {
+        console.log('Catched Error1', error);
+        console.log('Erreur en ecriture dans la page product.html');
     };
 };
-*/
 
-/*
-// fonction recupere id provenant de Url
-function idGetRequest() {
-
-    const var_str = window.location.href;
-    const var_url = new URL(var_str);
-    const var_search_params = new URLSearchParams(var_url.search);
-    const iobj_regex = new RegExp('^[a-zA-Z0-9]+$'); //  Une chaîne de caractères alpha-numérique en debut jusqu a fin
-
-    if (var_search_params.has('id')) {
-
-        const var_id = var_search_params.get('id');
-        console.log(var_id)
-
-        if (iobj_regex.test(var_id)) {
-            // appelle la fonction affichage du Detail Produit
-            console.log("id existant et au bon format");
-            return var_id;
-
-            // sinon sort avec un format incorrect
-        } else {
-            console.log("id avec format incorrect");
+/**
+ * Ajoute des ecouteurs a notre bouton Ajouter au panier
+ */
+function listenAddToCart() {
+    const var_btnCart = document.getElementById("addToCart");
+    var_btnCart.addEventListener("click", () => {
+        //creation de l object articleSelected si les conditions sont correctes
+        const obj_articleSelected = {
+            id: var_idUrl,
+            quantity: document.getElementById('quantity').value,                       //a simplifier ?
+            color: document.getElementById('colors').value,                            //a simplifier ?
+            name: document.getElementById('title').textContent                         //a simplifier ?
         };
-    };
+        console.log(obj_articleSelected);
+        //verifyInput(articleSelected);
+    });
 };
-*/
+// BIBLIOTHEQUE SPECIFIQUE - product.html - Fin  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+
+
+// Debut Partie en execution _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+//
+//
+// product.html - Appelle le contenu du produit selectionne de l API et insere dans la page
+if (testRouteSelectedProduct() == true) {
+    hubContent('routeSelectedProduct');
+};
+
+// index.html - Appelle le contenu de tous les produits de l API et insere dans la page
+if (testRouteAllProducts() == true) {
+    hubContent("routeAllProducts");
+};
--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,4 @@
+// CONFIGURATION GLOBALE
 const var_protocol = "http";
 const var_host = "://localhost"
 const var_port = ":3000";
@@ -6,6 +7,7 @@ const var_fullAddress = var_protocol + var_host + var_port + var_path;
 const var_allProducts = var_fullAddress + "/";
 
 
+// BIBLIOTHEQUE COMMUNE Debut _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 /**
  * Retourne du contenu de API
  * @param { string } param_path // qui est un chemin url
@@ -15,11 +17,47 @@ async function retrieveContent(param_path, param_keyAndValue) {
     if (param_keyAndValue == undefined) {
         param_keyAndValue = '';
     }
-    const response = await fetch(param_path + param_keyAndValue);
-    return response.json();
-    // !!! Demande du mentor : faire evoluer avec un timeout !!!
+    try {
+        const response = await fetch(param_path + param_keyAndValue);
+        return await response.json();
+    } catch (error) {
+        console.log('Catched Error5', error);
+        console.log('La reponse de API est en erreur avec ces parametres : ' + param_path + param_keyAndValue);
+    }
+}
+
+
+/**
+ * Identifie le bon itineraire vers SelectedProduct si le contexte url est valide
+ */
+function testRouteSelectedProduct() {
+    //reprend la valeur du parametre id dans Url
+    const var_idUrl = idGetRequest();
+    const testExpression = ("id=" + var_idUrl)
+    if (var_idUrl) {
+        //console.log('La page product.html contient bien un parametre id valide : ' + var_idUrl);
+        return true;
+    } else {
+        //console.log('La page product.html ne contient pas de parametre id valide : ' + var_idUrl);
+        return false;
+    }
 };
 
+/**
+ * Verifie que la page en cours est index.html
+ */
+function testRouteAllProducts() {
+    const myUrl = new URL(window.location);
+    const myRegex = new RegExp('index.html$');
+    const myUrlPathname = myUrl.pathname
+    //si trouve chaine
+    if (myRegex.test(myUrlPathname)) {
+        return true;
+    } else {
+        //console.log('La Regex echoue pour identifier la page index.html : ' + myUrlPathname);
+        return false;
+    }
+};
 
 /**
  * Utilise le bon iteneraire selon la demande
@@ -27,48 +65,63 @@ async function retrieveContent(param_path, param_keyAndValue) {
  */
 async function hubContent(routeAsked) {
     try {
-        if (routeAsked !== undefined) {
-            //recupere le contenu via iteneraire determinee par Api   
-            if (routeAsked == "routeSelectedProduct") {
-                //reprend la valeur du parametre id dans Url
-                const var_idUrl = idGetRequest();
-                if (var_idUrl != "" && var_idUrl != undefined) {
-                    hubContent(var_fullAddress + "/", "id=" + var_idUrl, "routeProduct");
-                    //cherche le contenu de API
-                    content = await retrieveContent(var_allProducts, var_idUrl);
-                    //ecrit le contenu
-                    writeSelectedProduct(content);
-                } else {
-                    console.log("id avec format incorrect ou vide");
-                }
-            }
-            if (routeAsked == "routeAllProducts") {
-                //cherche le contenu de API
-                content = await retrieveContent(var_allProducts);
-                //ecrit le contenu
-                writeAllProducts(content);
-            }
+        //recupere le contenu via iteneraire determinee par Api   
+        if (routeAsked == "routeSelectedProduct") {
+            //reprend la valeur du parametre id dans Url
+            const var_idUrl = idGetRequest();
+            content = await retrieveContent(var_allProducts, var_idUrl);
+            //ecrit le contenu
+            writeSelectedProduct(content);
+        }
+        if (routeAsked == "routeAllProducts") {
+            //cherche le contenu de API
+            content = await retrieveContent(var_allProducts);
+            //ecrit le contenu
+            writeAllProducts(content);
         }
     } catch (e) {
-        console.log('Catched Error', e);
-        console.log(routeAsked);
+        console.log('Catched Error4', e);
+        console.log('Erreur dans demande d itineraire ' + routeAsked);
     }
 };
 
+/**
+ * Recupere id provenant de Url et verifie son format
+ */
+function idGetRequest() {
+    const var_str = window.location.href;
+    const myUrl = new URL(var_str);
+    const var_search = new URLSearchParams(myUrl.search);
+    // une chaine de caracteres alpha-numerique en debut jusqu a fin
+    const myRegex = new RegExp('^[a-zA-Z0-9]+$');
+    //si parametre id existant dans url
+    if (var_search.has('id')) {
+        const var_id = var_search.get('id');
+        //si format id au norme
+        if (myRegex.test(var_id)) {
+            return var_id;
+        } else {
+            console.log('La valeur de id est incorrecte dans son format : ' + var_id);
+        }
+    }
+    return false;
+};
+// BIBLIOTHEQUE COMMUNE Fin _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
+
+// BIBLIOTHEQUE SPECIFIQUE - index.html - Debut  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 /**
  * Ecrit le contenu de tous les produits dans la page html
  * @param { array } array_textRequest
  */
 async function writeAllProducts(array_textRequest) {
     try {
-        //console.log(array_textRequest);
+        // for (var i = 0; i < array_textRequest.length; i++) {     //OLD SCHOOL
+        array_textRequest.forEach((item) => {
 
-        for (var i = 0; i < array_textRequest.length; i++) {
-        
             //lien
             const var_link = document.createElement('a');
-            var_link.setAttribute("href", "product.html?id=" + array_textRequest[i]._id);
+            var_link.setAttribute("href", "product.html?id=" + item._id);
             document.getElementById('items').appendChild(var_link);
 
             //article
@@ -77,29 +130,112 @@ async function writeAllProducts(array_textRequest) {
 
             //image
             const var_image = document.createElement('img');
-            var_image.setAttribute("src", array_textRequest[i].imageUrl);
-            var_image.setAttribute("alt", array_textRequest[i].altTxt);
+            var_image.setAttribute("src", item.imageUrl);
+            var_image.setAttribute("alt", item.altTxt);
             var_article.appendChild(var_image);
 
             //titre
             const var_title = document.createElement('h3');
             var_title.setAttribute("class", "productName");
-            var_title.innerHTML = array_textRequest[i].name;
+            var_title.innerHTML = item.name;
             var_article.appendChild(var_title);
 
             //description
             const var_description = document.createElement('p');
             var_description.setAttribute("class", "productDescription");
-            var_description.innerHTML = array_textRequest[i].description;
+            var_description.innerHTML = item.description;
             var_article.appendChild(var_description);
-            }
+        });
 
     } catch (e) {
-        console.log('Catched Error', e);
+        console.log('Catched Error2', e);
+        console.log('Erreur en ecriture dans la page index.html');
     }
 };
+// BIBLIOTHEQUE SPECIFIQUE - index.html - Fin  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+// BIBLIOTHEQUE SPECIFIQUE - product.html - Debut  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+/**
+ * Ecrit le contenu du produit selectionne dans la page html
+ * @param { array } array_selectedProduct
+ */
+async function writeSelectedProduct(array_selectedProduct) {
+    try {
+
+        //titre de la page html
+        document.title = array_selectedProduct.name;
+
+        //image
+        const var_image = document.createElement('img');
+        var_image.setAttribute("src", array_selectedProduct.imageUrl);
+        var_image.setAttribute("alt", array_selectedProduct.altTxt);
+        document.getElementsByClassName('item__img')[0].appendChild(var_image);         //a simplifier ?
+
+        //titre
+        const var_title = document.getElementById('title');                             //a simplifier ?
+        var_title.innerHTML = array_selectedProduct.name;
+
+        //prix
+        const var_price = document.getElementById('price');                             //a simplifier ?
+        var_price.innerHTML = array_selectedProduct.price;
+
+        //description
+        const var_description = document.getElementById('description');                //a simplifier ?
+        var_description.innerHTML = array_selectedProduct.description;
+
+        //couleurs
+        const var_eltHtmlSelect = array_selectedProduct.colors;
+        //boucle de remplissage des options de couleur
+        //for (var i = 0; i < var_eltHtmlSelect.length; i++) {     //OLD SCHOOL
+        var_eltHtmlSelect.forEach((item) => {
+            //pour une option
+            const var_color = document.createElement('option');
+            var_color.setAttribute("value", item);
+            var_color.innerHTML = item;
+            document.getElementById('colors').appendChild(var_color);                 //a simplifier ?
+        });
+
+        //quantite unitaire
+        const var_quantity = document.getElementById('quantity');                     //a simplifier ?
+        var_quantity.setAttribute("value", 1);
+
+    } catch (error) {
+        console.log('Catched Error1', error);
+        console.log('Erreur en ecriture dans la page product.html');
+    };
+};
+
+/**
+ * Ajoute des ecouteurs a notre bouton Ajouter au panier
+ */
+function listenAddToCart() {
+    const var_btnCart = document.getElementById("addToCart");
+    var_btnCart.addEventListener("click", () => {
+        //creation de l object articleSelected si les conditions sont correctes
+        const obj_articleSelected = {
+            id: var_idUrl,
+            quantity: document.getElementById('quantity').value,                       //a simplifier ?
+            color: document.getElementById('colors').value,                            //a simplifier ?
+            name: document.getElementById('title').textContent                         //a simplifier ?
+        };
+        console.log(obj_articleSelected);
+        //verifyInput(articleSelected);
+    });
+};
+// BIBLIOTHEQUE SPECIFIQUE - product.html - Fin  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
 
 
-// partie en execution
-// appelle le contenu des Produits de l API et insere Html
-hubContent("routeAllProducts");
+
+// Debut Partie en execution _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+//
+//
+// product.html - Appelle le contenu du produit selectionne de l API et insere dans la page
+if (testRouteSelectedProduct() == true) {
+    hubContent('routeSelectedProduct');
+};
+
+// index.html - Appelle le contenu de tous les produits de l API et insere dans la page
+if (testRouteAllProducts() == true) {
+    hubContent("routeAllProducts");
+};
